4bb5c2b5e049d44e1c4eda927f7b85c9898ed620
Maksym Niechaiev <34244452+niechaiev@users.noreply.github.com>
2026-07-18 18:06:35 +0300

    network, shared cursor, movable camera

---
+++ Assets/Scripts/ClientPlayerMove.cs
@@ -0,0 +1,59 @@
+using Unity.Cinemachine;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ClientPlayerMove : NetworkBehaviour
+{
+    [SerializeField] private InputActionReference rightClickAction;
+    [SerializeField] private InputActionReference lookAction;
+
+    [SerializeField] private GameObject phantomParent;
+    [SerializeField] private MeshRenderer[] phantomMeshRenderers;
+    [SerializeField] private Collider phantomPlaneCollider;
+
+    private CinemachineCamera _cinemachineCamera;
+    private CinemachineInputAxisController _cinemachineInputAxisController;
+
+    private void LateUpdate()
+    {
+        if (IsOwner)
+        {
+            phantomParent.transform.position = _cinemachineCamera.transform.position;
+            phantomParent.transform.rotation = _cinemachineCamera.transform.rotation;
+        }
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsOwner)
+        {
+            _cinemachineCamera = FindAnyObjectByType<CinemachineCamera>();
+            _cinemachineCamera.Target.TrackingTarget = transform;
+            _cinemachineCamera.Follow = transform;
+
+
+            _cinemachineInputAxisController = _cinemachineCamera.GetComponent<CinemachineInputAxisController>();
+            rightClickAction.action.started += _ =>
+            {
+                _cinemachineInputAxisController.Controllers[0].Input.InputAction = lookAction;
+                _cinemachineInputAxisController.Controllers[1].Input.InputAction = lookAction;
+            };
+            rightClickAction.action.canceled += _ =>
+            {
+                _cinemachineInputAxisController.Controllers[0].Input.InputAction = null;
+                _cinemachineInputAxisController.Controllers[1].Input.InputAction = null;
+            };
+            rightClickAction.action.Enable();
+
+            foreach (var meshRenderer in phantomMeshRenderers)
+            {
+                meshRenderer.enabled = false;
+            }
+        }
+        else
+        {
+            phantomPlaneCollider.enabled = false;
+        }
+    }
+}
---
+++ Assets/Scripts/SharedCursor.cs
@@ -0,0 +1,41 @@
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SharedCursor : NetworkBehaviour
+{
+    [SerializeField] private InputActionReference _pointAction;
+    private Camera _cam;
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsOwner)
+        {
+            _cam = Camera.main;
+            _pointAction.action.Enable();
+            GetComponent<Collider>().enabled = false;
+        }
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        if (IsOwner)
+        {
+            _pointAction.action.Disable();
+        }
+    }
+
+    private void Update()
+    {
+        if (IsOwner)
+        {
+            var screenPos = _pointAction.action.ReadValue<Vector2>();
+            if (screenPos == Vector2.zero) return;
+
+            var ray = _cam.ScreenPointToRay(screenPos);
+
+            if (Physics.Raycast(ray, out var hit))
+                gameObject.transform.position = hit.point;
+        }
+    }
+}
